7cec292e01b6de44aa20629fb392b0fa1f448de6
Your Name <you@example.com>
2016-08-18 21:04:39 -0400

    first submit

---
+++ fb.js
@@ -0,0 +1,6 @@
+// facebook app settings
+module.exports = {
+	'appID' : '1741399686118040',
+	'appSecret' : 'f857de92c9e9f1799431db7557f23ad8',
+	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+}
